91482c9081e3c7cbec3fffd6e62429e96ab9f9a8
Luka Lonec <luka.lonec@gmail.com>
2025-06-27 22:26:43 +0200

    feat: add drifting

--- player.gd
+++ player.gd
@@ -6,10 +6,16 @@ const TURN_SPEED = 2.0         # How fast the player rotates (adjust as needed)
 func _physics_process(delta):
 	# Rotate left/right using input
 	var turn_input = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-	rotation.y += turn_input * TURN_SPEED * delta
+	var drift_input = Input.get_action_strength( "ui_accept")
+	rotation.y += turn_input * (TURN_SPEED + 0.3*drift_input) * delta
 
 	# Constant forward movement in local z-axis
 	var forward_direction = -transform.basis.z.normalized()
-	velocity = forward_direction * SPEED
+	
+	var drift_direction = drift_input * turn_input * transform.basis.x.normalized()
+	velocity = (drift_direction + forward_direction).normalized() * SPEED
+	
+	
+		
 
 	move_and_slide()
