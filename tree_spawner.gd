225664a19358d176fc9097e12f58fea36ca4f281
Luka Lonec <luka.lonec@gmail.com>
2025-06-28 12:09:05 +0200

    make map round

--- tree_spawner.gd
+++ tree_spawner.gd
@@ -12,7 +12,7 @@ extends Node3D
 
 @export var tree_scale: float = 0.3
 
-@export var area_size: float = 300
+@export var area_size: float = 100
 @export var spacing: float = 1.5
 @export var noise_threshold: float = 0.6
 
@@ -43,10 +43,14 @@ func _ready():
 	noise.frequency = 0.5
 	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	  # Only spawn trees where noise is above this
-	var x = -area_size / 2.0
-	while x < area_size / 2.0:
-		var z = -area_size / 2.0
-		while z < area_size / 2.0:
+	var x = -area_size
+	while x < area_size:
+		var z = -area_size
+		while z < area_size:
+			if x*x + z*z > area_size*area_size:
+				z += spacing
+				continue
+			var diff = abs(x*x + z*z - area_size*area_size)
 			var world_x = float(x)
 			var world_z = float(z)
 
@@ -55,8 +59,11 @@ func _ready():
 			if value > noise_threshold && (abs(world_x) > starting_area ||  abs(world_z) > starting_area):
 				  # Replace with terrain height if needed
 				spawn_tree_at(Vector3(world_x, y, world_z))
-			else:
+			elif diff > 6000:
 				spawn_grass_at(Vector3(world_x, y, world_z))
+			elif value > noise_threshold/2:
+				spawn_grass_at(Vector3(world_x, y, world_z))
+			
 			z += spacing
 		x += spacing
 
